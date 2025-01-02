c48d9c8733d0fe152112f501f1d165ca13fb4793
rkawano518 <rkawano518@gmail.com>
2025-01-01 16:54:13 -0800

    Remove busy-waiting from log loop by adding a condition variable

--- src/log.cpp
+++ src/log.cpp
@@ -8,14 +8,23 @@ namespace rk {
 namespace log {
 
 /**
- * This function will continously check the log queue for new messages and
- * print them via std::cout. It will end the loop once the endLoop flag is set
- * to true and the queue is empty.
+ * Checks whether the log queue has messages or the endLoop flag is true. If either are true, it will return true,
+ * stop the condition variable from waiting, and resume execution of the log loop.
+ */
+bool condVarPredicate() {
+    std::unique_lock<std::mutex> endLoopLock(endLoopMtx);
+    return !logQueue.empty() || endLoop;
+}
+
+/**
+ * This function will continously check the log queue for new messages and print them via std::cout.
+ * If no logs are in the queue, it'll wait until new ones get added.
+ * It will end the loop once the endLoop flag is set to true and the queue is empty.
  */
 void logQueueLoop() {
     std::string msg;
     while (true) {
-        std::lock_guard<std::mutex> logLock(logQueueMutex);
+        std::unique_lock<std::mutex> logLock(logQueueMutex);
         if (!logQueue.empty()) {
             msg = logQueue.front();
             logQueue.pop();
@@ -25,10 +34,15 @@ void logQueueLoop() {
                 msg.clear();
             }
         }
-
-        std::lock_guard<std::mutex> endLoopLock(endLoopMtx);
-        if (endLoop && logQueue.empty()) {
-            break;
+        else {
+            std::unique_lock<std::mutex> endLoopLock(endLoopMtx);
+            if (!endLoop) {
+                endLoopLock.unlock();
+                cv.wait(logLock, rk::log::condVarPredicate);
+            }
+            else {
+                break;
+            }
         }
     }
 }
@@ -49,6 +63,7 @@ void endLogThread(std::thread& thread) {
     {
         std::lock_guard<std::mutex> lock(endLoopMtx);
         endLoop = true;
+        cv.notify_all();
     }
 
     if (thread.joinable()) {
