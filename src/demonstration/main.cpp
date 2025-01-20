a000487b7fff4b50b6560e11edb93385d970412b
rkawano518 <rkawano518@gmail.com>
2025-01-19 20:08:33 -0800

    Add some more example logs to the demonstration

--- src/demonstration/main.cpp
+++ src/demonstration/main.cpp
@@ -2,10 +2,11 @@
  * @file main.cpp
  * @brief Main file for demonstrating the use of the logger.
  */
+#include <thread>
+#include <random>
+
 #include "rk_logger/log.h"
 #include "file1.h"
-
-#include <thread>
 
 int main() {
     // Call this function to start the logger. Save the logging thread for when the program ends.
@@ -26,7 +27,43 @@ int main() {
     std::thread otherThread([] () {
         RK_LOG("Inside other thread\n");
     });
-    otherThread.join();
+    if (otherThread.joinable()) {
+        otherThread.join();
+    }
+
+    // Print messages on a timer
+    constexpr size_t MAX_ITERATIONS = 15;
+    size_t timerCount = 0;
+    std::thread timerThread([&MAX_ITERATIONS, &timerCount] () {
+        while (timerCount < MAX_ITERATIONS) {
+            RK_LOG("Printing a message every 3 seconds. Timer messages remaining: ", MAX_ITERATIONS - timerCount, "\n");
+            std::this_thread::sleep_for(std::chrono::seconds(3));
+            timerCount++;
+        }
+    });
+
+    // Generate some random events
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    size_t randomDuration;
+    constexpr size_t MAX_RANDOM_EVENTS = 15;
+    size_t randomEventsCount = 0;
+    std::thread randomEvents([&gen, &randomDuration, &randomEventsCount] () {
+        while (randomEventsCount < MAX_RANDOM_EVENTS) {
+            std::uniform_int_distribution<> dist(0, 5);
+            randomDuration = dist(gen);
+            RK_LOG("Random event happened. Printing a log for it. Random events remaining: ", MAX_RANDOM_EVENTS - randomEventsCount, "\n");
+            std::this_thread::sleep_for(std::chrono::seconds(randomDuration));
+            randomEventsCount++;
+        }
+    });
+
+    if (timerThread.joinable()) {
+        timerThread.join();
+    }
+    if (randomEvents.joinable()) {
+        randomEvents.join();
+    }
 
     // Call this at the end of the program to shutdown the logger
     rk::log::stopLogger(std::move(logThread));
